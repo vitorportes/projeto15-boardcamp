3a8ae437ed264a5c75ef03ecb8950cb97745ad67
Vitor Portes <vitorportnoy@gmail.com>
2022-10-03 12:25:31 -0300

    fix: port change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`server up on ${port}`);
